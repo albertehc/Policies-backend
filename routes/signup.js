2e1b8cbb5d84110426191b1a1f421524a031eceb
Diablobait <thor.stg@gmail.com>
2020-05-18 22:23:54 +0200

    .env example

--- routes/signup.js
+++ routes/signup.js
@@ -3,8 +3,6 @@ const router = express.Router();
 const signupController = require("./../controllers/signupController");
 const checkError = require("./../helpers/checkUser");
 
-// /api/signup/
-
 router.post("/", checkError(), signupController.signup);
 
 module.exports = router;
